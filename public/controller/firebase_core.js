96f4def1fd81fed5d4f34889c8d81350fe531146
MINGHON_LEE <mlee58@uco.edu>
2025-01-29 17:04:41 -0600

    init commit

---
+++ public/controller/firebase_core.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.2.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAFJhF_VvRghoONuXYqqe6FfJCycI-J0lM",
+    authDomain: "mlee58-cmsc5373-webapp.firebaseapp.com",
+    projectId: "mlee58-cmsc5373-webapp",
+    storageBucket: "mlee58-cmsc5373-webapp.firebasestorage.app",
+    messagingSenderId: "48325189388",
+    appId: "1:48325189388:web:3ddfe4c8db5ed83168b329"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
